2d90bc1d6880f7407ff593f65cc85d8aca5c22ae
Andrey  ARISTOV <aaristov@pasteur.fr>
2020-03-11 20:48:48 +0100

    add binning, stacking, repr

--- api/core.py
+++ api/core.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage.transform import downscale_local_mean
 
 class Well:
 
@@ -11,4 +12,44 @@ class Well:
         self.ndim = array.ndim
         self.shape = array.shape
         self.meta = meta
+
+    @classmethod
+    def stack(self, list_of_wells):
+        '''
+        Makes a stack from 1D list of wells
+        '''
+        arr = np.stack([w.array for w in list_of_wells])
+        try:
+            meta = {k: set([w.meta[k] for w in list_of_wells]) for k in list_of_wells[0].meta}
+        except TypeError:
+            meta = {k: [w.meta[k] for w in list_of_wells] for k in list_of_wells[0].meta}
+
+        return Well(arr, {**meta, 'stacked': len(list_of_wells)})
+
+    def bin(self, factor):
+        '''bin xy `factor` times'''
+        factors = np.ones_like(self.array.shape)
+        factors[-2:] = factor
+        arr = downscale_local_mean(self.array, tuple(factors))
+        return Well(arr, meta = {'binning': factor, **self.meta})
+
+    
+    def __repr__(self,):
+        try:
+            import matplotlib.pyplot as plt
+            if self.array.ndim == 2:
+                plt.imshow(self.array, cmap='gray')
+                # plt.title(self.meta)
+            elif self.array.ndim == 3:
+                plt.imshow(self.array[0], cmap='gray')
+                # plt.title(self.meta)
+                print('Showing first layer')
+            else:
+                pass
+
+
+        except Exception:
+            pass
+        finally:
+            return f'array {self.array.shape}, {self.meta}'
 
