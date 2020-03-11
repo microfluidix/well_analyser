3ba59dba50e716a597f62eb9273be6683b2847e5
ronteix <gronteix@pasteur.fr>
2020-03-11 14:58:59 +0100

    change import

--- tests/test_segment.py
+++ tests/test_segment.py
@@ -1,14 +1,11 @@
-from api import segment
+from api.segment import utilities
 import numpy as np
 
 def test_well():
     shape = (20,20)
     arr = np.zeros(shape)
 
-    arr[5:15, 10:20] = segment.utilities._makeDiskMask(10, 10, 1)
-
-    print(arr)
-
+    arr[5:15, 10:20] = utilities._make_circ_mask(10, 10, 1)
 
     #assert well.shape == shape
     #assert well.meta['shape'] == shape
