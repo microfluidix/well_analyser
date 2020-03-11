779eb4448e0bf00ea2d8767f66438b8a30a097f8
Andrey  ARISTOV <aaristov@pasteur.fr>
2020-03-11 14:57:38 +0100

    change import

--- tests/test_segment.py
+++ tests/test_segment.py
@@ -1,11 +1,12 @@
-from api import segment
+# from api.segment import utilities
+from api.segment import utilities
 import numpy as np
 
 def test_well():
     shape = (20,20)
     arr = np.zeros(shape)
 
-    arr[5:15, 10:20] = segment.utilities._makeDiskMask(10, 10, 1)
+    arr[5:15, 10:20] = utilities._makeDiskMask(10, 10, 1)
 
     print(arr)
 
