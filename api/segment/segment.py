8933555ecd01cf2f72da6441e697aded2bf5d154
ronteix <gronteix@pasteur.fr>
2020-03-11 13:25:38 +0100

    mport BF segment

--- api/segment/segment.py
+++ api/segment/segment.py
@@ -1,6 +1,18 @@
+import os
+import sys
+import numpy as np
+
+import pims
+from scipy.ndimage import gaussian_filter
+import matplotlib.pyplot as plt
+import cv2
+from numpy import unravel_index
+from scipy import ndimage, misc
+from skimage.measure import label, regionprops
 
 
-def _crop(imgToCrop:np.ndarray,
+
+def crop(imgToCrop:np.ndarray,
     imgToAnalyze:np.ndarray,
     maskSize:int,
     wellSize:int,
@@ -27,95 +39,107 @@ def _crop(imgToCrop:np.ndarray,
 
     return imgToCrop[int(startx):int(startx+cropDist),int(starty):int(starty+cropDist)]
 
-def _makeCircMask(maskSize:int,
-    wellSize:int,
-    muToPx:float):
+
+from api import core
+
+core.
+
+def _getSphCoords(PATH:str, experiment:str, time:str, CHANNEL:str,
+    wellDiameter:int, marginDistance:int, umToPx:float):
 
     """
+    CORE FUNCTION:
 
-    Makes a donut mask for future convolution.
+    Function to retrieve the spheroid coordinates from the BF images. Relies
+    upon ID by max gradient values.
 
     Returns:
-    - np.ndarray
+    - tuple_of_arrays
 
     """
 
-    cropDist = maskSize*muToPx
-
-    X = np.arange(cropDist)
-    Y = X
-
-    X, Y = np.meshgrid(X, Y)
-
-    mask = ((np.sqrt((X-cropDist//2)**2 + (Y-cropDist//2)**2) > (wellSize*muToPx)//2 - 10*muToPx) &
-            (np.sqrt((X-cropDist//2)**2 + (Y-cropDist//2)**2) < (wellSize*muToPx)//2 + 10*muToPx))
-
-    return mask.astype(np.int)
+    img = pims.ImageSequence(os.path.join(PATH, experiment, CHANNEL, '*.tif'),
+        as_grey=True)
+    imToCrop = img[int(time)]
 
 
-def _getCenter(imgToAnalyze:np.ndarray, maskSize:int, wellSize:int,
-    muToPx:float):
-
-    """
-
-    Find the well center of an image.
-
-    Returns:
-    - tuple
-
-    """
-
-    mask = _makeCircMask(maskSize,wellSize,muToPx)
-
-    conv = cv2.filter2D(imgToAnalyze, cv2.CV_32F, mask)
-
-    return unravel_index(conv.argmin(), conv.shape)
+    BFimage = cropper._centerSelect(imToCrop, wellDiameter,
+        marginDistance, umToPx)
+    rRegion = _findSpheroid(BFimage, wellDiameter, umToPx, marginDistance)
 
 
-def _centerSelect(imToCrop:np.ndarray, wellDiameter:int, marginDistance:int,
-    umToPx:float):
+    # Image the segmentation to keep intermediary result of the segmentation.
+    _verifySegmentationBF(BFimage, rRegion, PATH, experiment, time)
+
+    return np.nonzero(rRegion)
+
+
+##### Local utility functions #####
+
+
+def _findSpheroid(imCropped, wellDiameter, umToPx, marginDistance, fraction = 5,
+                  minRegionArea = 15000, maxRegionArea = 120000):
 
     """
 
-    Function to take image, find the well center,
-    throw away all the points beyond the marginDistance.
-
-    Returns:
-    - np.ndarray
+    We find the spheroid by thresholding the intensity
+    and area filling. Sph. must have a dark border around
+    it.
 
     """
 
-    img = _crop(imToCrop, imToCrop, wellDiameter, wellDiameter, umToPx)
 
-    mask = _makeDiskMask(wellDiameter, wellDiameter-marginDistance, umToPx)
-    t = np.zeros(np.shape(mask))
-    a, b = np.shape(img)
-    t[:a, :b] = img
-    #combine pour gerer les crops de forme non carree
+    result1 = ndimage.sobel(imCropped, 1)
+    result2 = ndimage.sobel(imCropped, 0)
 
-    finalIm = np.multiply(t, mask)
-    finalIm[finalIm == 0] = np.max(finalIm)
+    mask = cropper._makeDiskMask(wellDiameter, wellDiameter-marginDistance-20,
+        umToPx)
 
-    return gaussian_filter(finalIm, sigma=5)
+    a, b = np.shape(result1)
 
+    sobelMasked = np.multiply(mask[:a, :b], np.sqrt(result1**2+result2**2))
+    toThresh = gaussian_filter(sobelMasked, sigma=5)
 
-def _makeDiskMask(maskSize:int, diskSize:int, umToPx:float):
+    imThresh = toThresh > np.max(toThresh)/fraction
 
-    """
+    cnts, h = cv2.findContours(imThresh.astype('uint8'), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    temp = cv2.drawContours(imThresh.astype('uint8'), cnts, -1, (255,255,255), thickness=cv2.FILLED)
 
-    Makes a circular mask of given radius.
+    imLabel = label(temp)
 
-    Returns:
-    - np.ndarray
+    for region in regionprops(imLabel):
 
-    """
+        if region.area < minRegionArea:
+        #check it is inside or outside
 
-    cropDist = maskSize*umToPx
+            temp[imLabel == region.label] = 0
+            #region given same value as sph. border
 
-    X = np.arange(cropDist)
-    Y = X
-    X, Y = np.meshgrid(X, Y)
+        if region.area > maxRegionArea:
+        #check it is inside or outside
 
-    mask = (np.sqrt((X-cropDist//2)**2 + (Y-cropDist//2)**2) < (diskSize*umToPx)//2)
+            temp[imLabel == region.label] = 0
+            #region given same value as sph. border
 
-    return mask.astype(np.int)
+        if region.eccentricity > 0.8:
+        #check it is inside or outside
+
+            temp[imLabel == region.label] = 0
+            #region given same value as sph. border
+
+    return temp
+
+def _verifySegmentationBF(BFimage, rRegion, PATH, experiment, time):
+
+    if not os.path.exists(os.path.join(PATH, experiment, 'Spheroid Region Detection')):
+        os.makedirs(os.path.join(PATH, experiment, 'Spheroid Region Detection'))
+    savePath = os.path.join(PATH, experiment, 'Spheroid Region Detection')
+
+    fig, ax = plt.subplots(1,1, figsize = (10,10))
+
+    plt.imshow(BFimage, cmap='gray', origin = 'lower')
+    plt.imshow(rRegion, alpha = 0.1, origin = 'lower')
+    plt.savefig(os.path.join(savePath, 'testFrame_%frame.jpeg' %round(int(time,0))))
+    plt.close(fig)
+
+    return
---
+++ api/segment/utilities.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import numpy as np
+
+import pims
+from scipy.ndimage import gaussian_filter
+import matplotlib.pyplot as plt
+import cv2
+from numpy import unravel_index
+from scipy import ndimage, misc
+from skimage.measure import label, regionprops
+
+def _makeCircMask(maskSize:int,
+    wellSize:int,
+    muToPx:float):
+
+    """
+
+    Makes a donut mask for future convolution.
+
+    Returns:
+    - np.ndarray
+
+    """
+
+    cropDist = maskSize*muToPx
+
+    X = np.arange(cropDist)
+    Y = X
+
+    X, Y = np.meshgrid(X, Y)
+
+    mask = ((np.sqrt((X-cropDist//2)**2 + (Y-cropDist//2)**2) > (wellSize*muToPx)//2 - 10*muToPx) &
+            (np.sqrt((X-cropDist//2)**2 + (Y-cropDist//2)**2) < (wellSize*muToPx)//2 + 10*muToPx))
+
+    return mask.astype(np.int)
+
+
+def _getCenter(imgToAnalyze:np.ndarray, maskSize:int, wellSize:int,
+    muToPx:float):
+
+    """
+
+    Find the well center of an image.
+
+    Returns:
+    - tuple
+
+    """
+
+    mask = _makeCircMask(maskSize,wellSize,muToPx)
+
+    conv = cv2.filter2D(imgToAnalyze, cv2.CV_32F, mask)
+
+    return unravel_index(conv.argmin(), conv.shape)
+
+
+def _centerSelect(imToCrop:np.ndarray, wellDiameter:int, marginDistance:int,
+    umToPx:float):
+
+    """
+
+    Function to take image, find the well center,
+    throw away all the points beyond the marginDistance.
+
+    Returns:
+    - np.ndarray
+
+    """
+
+    img = _crop(imToCrop, imToCrop, wellDiameter, wellDiameter, umToPx)
+
+    mask = _makeDiskMask(wellDiameter, wellDiameter-marginDistance, umToPx)
+    t = np.zeros(np.shape(mask))
+    a, b = np.shape(img)
+    t[:a, :b] = img
+    #combine pour gerer les crops de forme non carree
+
+    finalIm = np.multiply(t, mask)
+    finalIm[finalIm == 0] = np.max(finalIm)
+
+    return gaussian_filter(finalIm, sigma=5)
+
+
+def _makeDiskMask(maskSize:int, diskSize:int, umToPx:float):
+
+    """
+
+    Makes a circular mask of given radius.
+
+    Returns:
+    - np.ndarray
+
+    """
+
+    cropDist = maskSize*umToPx
+
+    X = np.arange(cropDist)
+    Y = X
+    X, Y = np.meshgrid(X, Y)
+
+    mask = (np.sqrt((X-cropDist//2)**2 + (Y-cropDist//2)**2) < (diskSize*umToPx)//2)
+
+    return mask.astype(np.int)
