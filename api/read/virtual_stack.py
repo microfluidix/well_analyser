81dab035c7d401a35e2456a5a518d7e215e82225
Andrey  ARISTOV <andrey.aristov@pasteur.frsource>
2020-03-14 00:54:43 +0100

    format

--- api/read/virtual_stack.py
+++ api/read/virtual_stack.py
@@ -10,7 +10,7 @@ from skimage.transform import downscale_local_mean
 class VirtualStack:
 
     '''Handle tif exports from NIS'''
-    
+
     def __init__(self, folder:str, search_names:str='*.tif', regex:str=r'([ctmz])(\d{1,2})'):
         self.folder = folder
         self.flist = glob(os.path.join(folder, search_names))
@@ -54,7 +54,7 @@ class VirtualStack:
             if bin > 1:
                 img = img.bin(bin)
             yield img
-      
+
     def get_single_image(self, **kwargs) -> Well:
         '''reads .tif from disk, returns api.core.Well instance'''
         args = {ax: self._check_range(v, ax)[0] for ax, v in kwargs.items()}
@@ -67,13 +67,13 @@ class VirtualStack:
             raise ValueError(f'File not found for coordinates {args}')
         fname = os.path.basename(path)
         return Well(imread(path),  meta={**args, 'path': fname, 'prefix': self.folder})
-    
+
     def _check_range(self, values:tuple, axis:str):
         '''
         Check range values and generate range
-        
+
         '''
-        
+
         def single_check(value, axis=axis, upper:bool=False):
             side = ['min', 'max'][int(upper)]
             sr = self.ranges[axis]
@@ -82,13 +82,13 @@ class VirtualStack:
             assert isinstance(value, int)
             assert sr['min'] <= value <= sr['max'], f'{value} out of range for {axis}: {sr}'
             return value
-            
+
         if values is None:
             return range(single_check(None, upper=False), single_check(None, upper=True) + 1) 
 
         elif isinstance(values, int):
             return (single_check(values),)
-        
+
         elif isinstance(values, tuple):
             assert len(values) == 2
             return range(single_check(values[0], upper=False), single_check(values[1], upper=True) + 1)
@@ -96,8 +96,8 @@ class VirtualStack:
             raise ValueError(f'range {values} not understood, provide int or tuple')
 
     def __repr__(self):
-        return f'Virtial Stack instance. \nFound {len(self.flist)} files in {self.folder}. Ranges: {self.ranges}'
-   
+        return f'Virtial Stack instance. \nFound {len(self.flist)} files in {self.folder}. \nRanges: {self.ranges}'
+
 
 def get_indices(fname:str, regexp=r'([ctmz])(\d{1,2})') -> dict:
     '''
@@ -119,4 +119,3 @@ def get_sizes(indices:dict, order='tzc'):
     _min = tzc.min(axis=0)
     ranges = {o: {'min': low, 'max': high} for o, low, high in zip(order, _min, _max)}
     return ranges
-    
