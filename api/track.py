f922686dac172d4a520fff1e8398ea63a2cef97f
Gustave Ronteix <gronteix@users.noreply.github.com>
2020-03-24 20:48:59 +0100

    exception update

--- api/track.py
+++ api/track.py
@@ -87,63 +87,67 @@ def get_spheroid_properties(VirtualStack,
 
     for img in c1_time_reader:
 
-        print(img.meta)
-        
-        t = img.meta['t']
-        m = img.meta['m']
+        try:
 
-        (xc, yc) = utilities._get_center(img.array,wellSizeMu,wellSizeMu,muTopx)
-
-        # function to be changed to Andrey's version
-        crop_img_BF = segment.select_well(img.array, img.array, wellSizeMu, wellSizeMu, muTopx)            
-        sph_img = segment.find_spheroid(crop_img_BF, wellSizeMu, muTopx)
-
-        if get_fluo:
-
-            img_Fluo = VirtualStack.get_single_image(m=m, t=t, c=fluo_channel)
-
-
-            crop_img_Fluo = segment.select_well(img.array, 
-                img_Fluo.array, wellSizeMu, wellSizeMu, muTopx)
-
-            timeFrame = analyse.spheroid_properties(sph_img, crop_img_Fluo)
-        
-        else:
+            print(img.meta)
             
-            timeFrame = analyse.spheroid_properties(sph_img)
-        
-        
-        timeFrame['t'] = int(t)
-        timeFrame['m'] = int(m)
-        timeFrame['well_center_x'] = xc
-        timeFrame['well_center_y'] = yc
-        timeFrame['mask_size'] = wellSizeMu*muTopx
+            t = img.meta['t']
+            m = img.meta['m']
 
-        folder = VirtualStack.folder
+            (xc, yc) = utilities._get_center(img.array,wellSizeMu,wellSizeMu,muTopx)
 
-        if not os.path.exists(os.path.join(folder, 'spheroid_data_frame')):
-            os.makedirs(os.path.join(folder, 'spheroid_data_frame'))
+            # function to be changed to Andrey's version
+            crop_img_BF = segment.select_well(img.array, img.array, wellSizeMu, wellSizeMu, muTopx)            
+            sph_img = segment.find_spheroid(crop_img_BF, wellSizeMu, muTopx)
 
-        timeFrame.to_csv(os.path.join(os.path.join(folder, 
-            'spheroid_data_frame',
-            str(m) + '_' + str(t))))
-        
-        spheroid_frame = spheroid_frame.append(timeFrame)
+            if get_fluo:
 
-        # Verify segmentation
+                img_Fluo = VirtualStack.get_single_image(m=m, t=t, c=fluo_channel)
 
-        if verify_seg:
+
+                crop_img_Fluo = segment.select_well(img.array, 
+                    img_Fluo.array, wellSizeMu, wellSizeMu, muTopx)
+
+                timeFrame = analyse.spheroid_properties(sph_img, crop_img_Fluo)
+            
+            else:
+                
+                timeFrame = analyse.spheroid_properties(sph_img)
+            
+            
+            timeFrame['t'] = int(t)
+            timeFrame['m'] = int(m)
+            timeFrame['well_center_x'] = xc
+            timeFrame['well_center_y'] = yc
+            timeFrame['mask_size'] = wellSizeMu*muTopx
 
             folder = VirtualStack.folder
 
-            if not os.path.exists(os.path.join(folder, 'verify segmentation')):
-                os.makedirs(os.path.join(folder, 'verify segmentation'))
+            if not os.path.exists(os.path.join(folder, 'spheroid_data_frame')):
+                os.makedirs(os.path.join(folder, 'spheroid_data_frame'))
 
-            verify_segmentation.verifySegmentationBF(crop_img_BF,
-                sph_img,
-                os.path.join(folder, 'verify segmentation'),
-                int(m),
-                int(t))
+            timeFrame.to_csv(os.path.join(os.path.join(folder, 
+                'spheroid_data_frame',
+                str(m) + '_' + str(t))))
+            
+            spheroid_frame = spheroid_frame.append(timeFrame)
+
+            # Verify segmentation
+
+            if verify_seg:
+
+                folder = VirtualStack.folder
+
+                if not os.path.exists(os.path.join(folder, 'verify segmentation')):
+                    os.makedirs(os.path.join(folder, 'verify segmentation'))
+
+                verify_segmentation.verifySegmentationBF(crop_img_BF,
+                    sph_img,
+                    os.path.join(folder, 'verify segmentation'),
+                    int(m),
+                    int(t))
+
+        except Exception as e: print(e)
 
     return spheroid_frame
 
