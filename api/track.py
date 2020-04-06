f85f70abc26c8c2c1d0a379841b842ecd1c9b28c
Gustave Ronteix <gronteix@users.noreply.github.com>
2020-04-06 12:43:04 +0200

    adapted trackframe

--- api/track.py
+++ api/track.py
@@ -11,13 +11,14 @@ from api.segment import verify_segmentation
 import api.segment.utilities as utilities
 
 
-def get_image_array(vs, 
-    m:int, 
-    fluo_channel:int):
+def make_tracks(track_frame:pandas.DataFrame):
 
-    fnames = f'm{m:02d}t*c{fluo_channel}.tif'
+    for m in track_frame['m'].unique():
+
+        track_frame.loc[track_frame['m'] == m] = trackpy.link(track_frame[track_frame['m'] == m],
+            search_range = 40)
     
-    return pims.open(os.path.join(vs.folder, fnames))
+    return track_frame
 
 def get_cell_tracks(vs,
     fluo_channel:int,
@@ -38,28 +39,29 @@ def get_cell_tracks(vs,
 
     min_size = (2*muTopx*minsize//2)+1
     track_frame = pandas.DataFrame()
-    analyzed_list = []
 
-    m_list = np.arange(vs.ranges['m']['min'],vs.ranges['m']['max']+1)
+    c2_time_reader = vs.read(t = None, 
+        m = None, 
+        c = fluo_channel)
 
-    for m in m_list:
+    for img in c2_time_reader:
 
-        if m not in analyzed_list:
+        t = img.meta['t']
+        m = img.meta['m']
         
-            # the parameter values returned by vs.ranges is a str
-            # but the input of vs.read is an int.
-            well_frame = trackpy.batch(get_image_array(vs, int(m), fluo_channel), 
-                min_size, 
-                minmass=minmass,
-                percentile = percentile)
+        # the parameter values returned by vs.ranges is a str
+        # but the input of vs.read is an int.
+        well_frame = trackpy.locate(img.array, 
+            min_size, 
+            minmass=minmass,
+            percentile = percentile)
 
-            well_frame['m'] = m
+        well_frame['m'] = m
+        well_frame['frame'] = t
 
-            track_frame = track_frame.append(well_frame)
+        track_frame = track_frame.append(well_frame)
 
-            analyzed_list.append(m)
-
-    return track_frame
+    return make_tracks(track_frame)
 
 def get_spheroid_properties(vs,
     spheroid_channel:int,
@@ -151,82 +153,3 @@ def get_spheroid_properties(vs,
         except Exception as e: print(e)
 
     return spheroid_frame
-
-def get_OT1_properties(vs,
-    OT1_frame:pandas.DataFrame,
-    spheroid_channel:int,
-    fluo_channel = None,
-    verify_seg = False,
-    wellSizeMu = 430,
-    muTopx = 3):
-
-    """
-    
-    Asses the state of OT1 cells wrt the spheroid.
-
-    COMMENT: with meta data we will integrate the above
-    variables implicitely.
-
-    Returns:
-     - pandas.DataFrame
-    
-    """
-
-    spheroid_frame = pandas.DataFrame()
-
-    c1_time_reader = vs.read(t = None, 
-        m = None, 
-        c = spheroid_channel)
-
-    for img in c1_time_reader:
-
-        try:
-
-            print(img.meta)
-            
-            t = img.meta['t']
-            m = img.meta['m']
-
-            (xc, yc) = utilities._get_center(img.array,wellSizeMu,wellSizeMu,muTopx)
-
-            # function to be changed to Andrey's version
-            crop_img_BF = segment.select_well(img.array, img.array, wellSizeMu, wellSizeMu, muTopx)            
-            sph_img = segment.find_spheroid(crop_img_BF, wellSizeMu, muTopx)
-
-            timeFrame = analyse.spheroid_properties(sph_img)
-
-            OT1_frame.loc[(OT1_frame['frame'] == t) & 
-                (OT1_frame['m'] == m), 'well_center_x']
-            
-            
-
-            folder = vs.folder
-
-            if not os.path.exists(os.path.join(folder, 'spheroid_data_frame')):
-                os.makedirs(os.path.join(folder, 'spheroid_data_frame'))
-
-            timeFrame.to_csv(os.path.join(os.path.join(folder, 
-                'spheroid_data_frame',
-                str(m) + '_' + str(t))))
-            
-            spheroid_frame = spheroid_frame.append(timeFrame)
-
-            # Verify segmentation
-
-            if verify_seg:
-
-                folder = vs.folder
-
-                if not os.path.exists(os.path.join(folder, 'verify segmentation')):
-                    os.makedirs(os.path.join(folder, 'verify segmentation'))
-
-                verify_segmentation.verifySegmentationBF(crop_img_BF,
-                    sph_img,
-                    os.path.join(folder, 'verify segmentation'),
-                    int(m),
-                    int(t))
-
-        except Exception as e: print(e)
-
-    return spheroid_frame
-
